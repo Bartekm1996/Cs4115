3a208df657dd4c61c42cc32592cf171cdedac7bf
Bartekm1996 <35643353+Bartekm1996@users.noreply.github.com>
2019-02-28 17:28:18 +0000

    Update and rename Labs/Week 3/Multmax.cc to Labs/Week03/matmult.cc

--- Labs/Week03/matmult.cc
+++ Labs/Week03/matmult.cc
@@ -6,38 +6,31 @@ void initArray(double**,int,int);
 void populateArray(double**,int,int);
 void printArray(double**,int,int);
 void arrayResult(double**,double**,int,int,int,int);
-//double product(double**,double**,int,int,int);
 
 
 int main(int argc, char **argv){
 
     if(argc != 5){
+        
         std::cerr << "Error :  5 arguments required - Dim of Mat A & Dim of Mat B" << std::endl;
         return -1;
+        
     }else {
         if(validateMatrixInputSize(argv)){
                 
             int rows_no_matrix_a = atoi(*(argv+1)), rows_no_matrix_b = atoi(*(argv+3));
             int cols_no_matrix_a = atoi(*(argv+2)), cols_no_matrix_b = atoi(*(argv+4));
-
-            //std::cout << "Size of matrix A " << rows_no_matrix_a << "\t" << cols_no_matrix_a << std::endl;
-            //std::cout << "Size of matrix B " << rows_no_matrix_b << "\t" << cols_no_matrix_b << std::endl;
-
-                double **arrayA = new double*[rows_no_matrix_a];
-                double **arrayB = new double*[rows_no_matrix_b];
                 
-                initArray(arrayA,rows_no_matrix_a,cols_no_matrix_a);
-                initArray(arrayB,rows_no_matrix_b,cols_no_matrix_b);
+            double **arrayA = new double*[rows_no_matrix_a];
+            double **arrayB = new double*[rows_no_matrix_b];
+                
+            initArray(arrayA,rows_no_matrix_a,cols_no_matrix_a);
+            initArray(arrayB,rows_no_matrix_b,cols_no_matrix_b);
 
-            //std::cout << "Enter numbers of matrix A " << std::endl;
-                    populateArray(arrayA,rows_no_matrix_a,cols_no_matrix_a);
-                    //printArray(arrayA,rows_no_matrix_a,cols_no_matrix_a);
+            populateArray(arrayA,rows_no_matrix_a,cols_no_matrix_a);
+            populateArray(arrayB,rows_no_matrix_b,cols_no_matrix_b);
 
-            //std::cout<<"Enter number of matrix B" << std::endl;
-                    populateArray(arrayB,rows_no_matrix_b,cols_no_matrix_b);
-                    //printArray(arrayB,rows_no_matrix_b,cols_no_matrix_b);
-
-                    arrayResult(arrayA,arrayB,rows_no_matrix_a,cols_no_matrix_b,rows_no_matrix_b,cols_no_matrix_a);
+            arrayResult(arrayA,arrayB,rows_no_matrix_a,cols_no_matrix_b,rows_no_matrix_b,cols_no_matrix_a);
 
         }else{ 
             std::cerr << "Error : Matrix size incomaptable" << std::endl;
@@ -81,17 +74,10 @@ void printArray(double **array, int rows_no, int cols_no){
 
 void arrayResult(double **arrayA, double **arrayB, int rows_no_mat_a, int cols_no_mat_b,int rows_no_mat_b,int cols_no_mat_a){
 
-    /*
-        double **array = new double*[rows_no_mat_a];
-        initArray(array,rows_no_mat_a,cols_no_mat_b);
-        Since the resultant array isnt going to be reused again, its sufficent to just print the output directly
-    */
-
         double product = 0;
         for(int i = 0; i < rows_no_mat_a; i++){
             for(int j = 0; j < cols_no_mat_b; j++){
                 for(int k = 0; k < cols_no_mat_a; k++){
-                    //array[i][j] = product(arrayA,arrayB,i,cols_no_mat_a,j);
                     product += (arrayA[i][k]*arrayB[k][j]);
                 }
                 std::cout << product;
@@ -102,10 +88,6 @@ void arrayResult(double **arrayA, double **arrayB, int rows_no_mat_a, int cols_n
             std::cout << std::endl;
         }
 
-        /*
-            Pass the resultant array to the printArray method
-            printArray(array,rows_no_mat_a,cols_no_mat_b);
-        */
 }
 
 /*
